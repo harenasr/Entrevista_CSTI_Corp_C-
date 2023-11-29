83245525d3479b826f27bc22220174e27392695b
Henry <henry.arenas.ramirez.contractor@bbva.com>
2023-11-28 19:57:15 -0500

    Agregando proyecto de lectura de serial en Csharp

---
+++ Entrevista_CSTI_Corp_C/SerialExampleCSharp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace Entrevista_CSTI_Corp_C
+{
+    internal class SerialExampleCSharp
+    {
+        static void Main()
+        {
+            SerialPort serialPort = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+
+            try
+            {
+                serialPort.Open();
+                Console.WriteLine("Esperando mensaje...");
+
+                string receivedMessage = serialPort.ReadLine();
+                Console.WriteLine("Mensaje recibido: " + receivedMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                serialPort.Close();
+            }
+        }
+    }
+}
